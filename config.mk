1c21da909764c2535df71f411c2ff9f0ea71158d
David Cantrell <david.l.cantrell@gmail.com>
2021-04-30 11:47:31 -0400

    Port users(1) from FreeBSD

--- config.mk
+++ config.mk
@@ -1,16 +1,20 @@
-CC = gcc
+CC        = gcc
+CXX       = g++
 
 # Standard compiler flags
-CFLAGS = -Wall -O2 -g
+CFLAGS    = -Wall -O2 -g
 
 # Define some macros to eliminate some FreeBSD-specifics in the code
-CFLAGS += -Dlint
-CFLAGS += -D'__FBSDID(x)='
-CFLAGS += -D__dead2=
-CFLAGS += -D__unused=
+CFLAGS   += -Dlint
+CFLAGS   += -D'__FBSDID(x)='
+CFLAGS   += -D__dead2=
+CFLAGS   += -D__unused=
+
+# Use the C flags for C++ code
+CXXFLAGS  = $(CFLAGS)
 
 # Where to install things
-DESTDIR ?=
-BINDIR  ?= /usr/bin
-SBINDIR ?= /usr/sbin
-MANDIR  ?= /usr/share/man
+DESTDIR  ?=
+BINDIR   ?= /usr/bin
+SBINDIR  ?= /usr/sbin
+MANDIR   ?= /usr/share/man
