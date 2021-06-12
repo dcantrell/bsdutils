fc51bf8a82b78517a7076d21363e27e3215dcca5
Daniel Kolesa <daniel@octaforge.org>
2021-06-12 17:46:05 +0200

    Install missing symlinks, do not install test twice
    Also fix test's install path missing a slash (which would result
    in it being installed in e.g. /usr/localbin instead of in
    /usr/local/bin)

--- src/test/install.sh
+++ src/test/install.sh
@@ -2,5 +2,6 @@
 
 BINDIR="${1}"
 
-mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}${BINDIR}"
-install -D -m 0755 ${MESON_BUILD_ROOT}/src/test/xtest "${DESTDIR}/${MESON_INSTALL_PREFIX}${BINDIR}"/test
+mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
+install -D -m 0755 ${MESON_BUILD_ROOT}/src/test/xtest "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/test
+ln -sf test "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}/["
