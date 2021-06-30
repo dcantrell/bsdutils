72302fb9934b2607652d49e9e0fc9cc002a4760b
Daniel Kolesa <daniel@octaforge.org>
2021-06-30 22:11:41 +0200

    stty: translate baud rates and speeds for printing and reading

--- src/stty/extern.h
+++ src/stty/extern.h
@@ -42,4 +42,7 @@ void	optlist(void);
 void	print(struct termios *, struct winsize *, int, enum FMT);
 void	usage(void) __dead2;
 
+int get_baud(speed_t s);
+speed_t get_speed(unsigned long b);
+
 extern struct cchar cchars1[], cchars2[];
