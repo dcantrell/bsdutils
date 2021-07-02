303d33162cdcc98c4f39143b194956ad74267f1a
Daniel Kolesa <daniel@octaforge.org>
2021-07-02 02:15:14 +0200

    Fix installation with bsdutils's own install(1)

--- src/test/install.sh
+++ src/test/install.sh
@@ -3,5 +3,6 @@
 BINDIR="${1}"
 
 mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
-install -D -m 0755 ${MESON_BUILD_ROOT}/src/test/xtest "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/test
+install -d "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
+install -m 0755 ${MESON_BUILD_ROOT}/src/test/xtest "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/test
 ln -sf test "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}/["
--- src/xinstall/install.sh
+++ src/xinstall/install.sh
@@ -3,4 +3,5 @@
 BINDIR="${1}"
 
 mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
-install -D -m 0755 ${MESON_BUILD_ROOT}/src/xinstall/xinstall "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/install
+install -d "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
+install -m 0755 ${MESON_BUILD_ROOT}/src/xinstall/xinstall "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/install
