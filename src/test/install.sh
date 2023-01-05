1975f53182709f145fee531b84fba36bf9200b5f
David Cantrell <dcantrell@redhat.com>
2023-01-05 14:58:24 -0500

    Fix installation of test(1) and the [(1) symlink

--- src/test/install.sh
+++ src/test/install.sh
@@ -5,6 +5,3 @@ BINDIR="${1}"
 mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
 install -d "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
 install -m 0755 ${MESON_BUILD_ROOT}/src/test/xtest "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/test
-ln -sf test "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}/["
-install -d "${DESTDIR}/${MESON_INSTALL_PREFIX}/${MANDIR}"/man1
-ln -sf test.1 "${DESTDIR}/${MESON_INSTALL_PREFIX}/${MANDIR}"/man1/"[.1"
